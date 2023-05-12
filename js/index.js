a58ea89e05e05c633ead1975d9c772624ac96b3b
Mason Bybee <bybeemason@gmail.com>
2023-05-12 16:00:28 -0500

    add porfolio to repository

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const navBar = document.querySelector('.navOl')
+document.addEventListener('scroll', function() {
+    let scrollcheck = window.scrollY;
+    if(scrollcheck !== 0) {
+        navBar.classList.add('scrolled')
+    }
+    else{navBar.classList.remove('scrolled')
+    }
+})
